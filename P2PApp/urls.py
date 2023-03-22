906975484285f04280ea530ddef13ad758b3ab92
Leandro Montero <39351969+leanml03@users.noreply.github.com>
2023-03-22 07:51:51 -0600

    first commit

---
+++ P2PApp/urls.py
@@ -0,0 +1,28 @@
+"""P2PApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from P2PApp.views import *
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/',register),
+    path('register/guardar-json/',guardar_json,name='guardar-json'),
+    path('home/',home, name='home'),
+    path('welcome/',welcome),
+    path('create_course/',create_course,name='create_course'),
+    path('create_course/reg_course/',reg_course,name='reg_course')
+]
---
+++ P2PApp/views.py
@@ -0,0 +1,87 @@
+import os
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+#from django.views.decorators.csrf import csrf_exempt
+#Aqui se van a crear las vistas //Retornan una respuesta http.
+
+#Request para realizar peticiones
+
+#HTTP Response Para enviar la respuesta usando el protocolo HTTP.
+
+#Esto es una vista
+def welcome(request):
+	return HttpResponse("<p style='color: red;'>Bienvenido<p>")
+def categoriaEdad(request,edad):
+	if edad>=18:
+		if edad>=60:
+			categoria="Tercera Edad"
+		else:
+			categoria="Adultez"
+	else:
+		categoria="Infante/Adolescente"
+	resultado="<h1>Categoria de la Edad: %s</h1>" %categoria
+	return HttpResponse(resultado)
+
+def home(request):
+	return render(request,'home.html')
+
+def register(request):
+	"""openTemplate=open(os.path.abspath("P2PApp/templates/register.html")) #Se llama a la ruta importando OS para poder correrlo en cualquier computador
+	template=Template(openTemplate.read()) #Se lee el HTML y se guarda en template (Se usa Template de libreria de django)
+	openTemplate.close() #Se cierra la obtencion del template para no dejarlo vulnerable
+	context=Context() #Objeto que nos permite indicar cuales objetos y variables va a utilizar la plantilla
+	document=template.render(context) ##Se renderiza el contexto
+	return HttpResponse(document) ##Se devuelve el html como respuesta"""
+	return render(request,'register.html')
+
+def welcome(request):
+	"""openTemplate=open(os.path.abspath("P2PApp/templates/welcome.html")) #Se llama a la ruta importando OS para poder correrlo en cualquier computador
+	template=Template(openTemplate.read()) #Se lee el HTML y se guarda en template (Se usa Template de libreria de django)
+	openTemplate.close() #Se cierra la obtencion del template para no dejarlo vulnerable
+	context=Context() #Objeto que nos permite indicar cuales objetos y variables va a utilizar la plantilla
+	document=template.render(context) ##Se renderiza el contexto
+	return HttpResponse(document) ##Se devuelve el html como respuesta"""
+	return render(request,'welcome.html')
+
+
+import json
+def guardar_json(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        with open('data/user', 'w') as f:
+            json.dump(data,f)
+	    #return render(request,'home.html')
+        return HttpResponse(status=200)
+		#return redirect('home.html')
+		
+    else:
+        return HttpResponse(status=400)
+    
+def guardar_json(request):
+	if request.method=='POST':
+		data=json.loads(request.body.decode('utf-8'))
+		with open('data/user.json','w') as f:
+			json.dump(data,f)
+		return HttpResponseRedirect('/home/')
+	else:
+		return HttpResponse(status=400)
+def create_course(request): #Ventana en donde se va a crear el curso
+	return render(request,'crear_curso.html')
+import uuid
+def reg_course(request): #Ventana en donde se va a crear el curso
+	if request.method=='POST':
+		data=json.loads(request.body.decode('utf-8'))
+		nombre_archivo= str(uuid.uuid4())+'.json'
+		ruta_archivo = os.path.join('data/courses/', nombre_archivo)
+		with open(ruta_archivo,'w') as f:
+			json.dump(data,f)
+		return HttpResponseRedirect('/home/')
+	else:
+		return HttpResponse(status=400)
+
+
+	
